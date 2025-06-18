e7576ca60f406066acd950f55a59f84d0018b3b9
erdemy <erdemy885@gmail.com>
2025-06-17 22:47:29 -0400

    finish test for torrentfile

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/erdemy885/turkTorrent
 
 go 1.24.4
 
-require github.com/jackpal/bencode-go v1.0.2
+require (
+	github.com/jackpal/bencode-go v1.0.2
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- torrent/torrent_test.go
+++ torrent/torrent_test.go
@@ -1,17 +1,39 @@
 package torrent
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"os"
-	"strconv"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var tf TorrentFile
 
+func openJSONTorrent(t *testing.T) bencodeTorrent {
+	file, err := os.Open("test-data/archlinux-2019.12.01-x86_64.iso.json")
+	assert.Nil(t, err)
+	defer file.Close()
+
+	fileStats, err := file.Stat()
+	assert.Nil(t, err)
+
+	fileBuffer := make([]byte, fileStats.Size())
+	bytesRead, err := file.Read(fileBuffer)
+	assert.Nil(t, err)
+	assert.Equal(t, fileStats.Size(), int64(bytesRead))
+
+	var jsonTorrent bencodeTorrent
+	json.Unmarshal(fileBuffer, &jsonTorrent)
+	return jsonTorrent
+}
+
 func TestMain(m *testing.M) {
 	var err error
-	tf, err = Open("test-data\\archlinux-2019.12.01-x86_64.iso.torrent")
+	tf, err = Open("test-data/archlinux-2019.12.01-x86_64.iso.torrent")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -20,16 +42,31 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-// TO-DO: compare outputs to json
-func TestPieceHashes(t *testing.T) {
-	fmt.Println(tf)
-	for i, hashes := range tf.PieceHashes {
-		fmt.Print(strconv.Itoa(i) + ":")
-		for _, hash := range hashes {
-			fmt.Printf(" %02X", hash)
-		}
-		fmt.Println()
-	}
-}
+func TestTorrent(t *testing.T) {
+	jsonTorrent := openJSONTorrent(t)
 
-//TO-DO: write testcases for each field of TorrentFile
+	assert.Equal(t, tf.Announce, jsonTorrent.Announce)
+	assert.Equal(t, tf.Name, jsonTorrent.Info.Name)
+	assert.Equal(t, tf.Length, jsonTorrent.Info.Length)
+	assert.Equal(t, tf.PieceLength, jsonTorrent.Info.PieceLength)
+
+	splitPieces := strings.Split(jsonTorrent.Info.Pieces, " ")
+	jsonTorrent.Info.Pieces = strings.Join(splitPieces, "")
+	pieces, err := hex.DecodeString(jsonTorrent.Info.Pieces)
+	jsonTorrent.Info.Pieces = string(pieces)
+	assert.Nil(t, err)
+
+	infoHash, err := jsonTorrent.Info.hash()
+	assert.Equal(t, tf.InfoHash, infoHash)
+	assert.Nil(t, err)
+
+	pieceHashes, err := jsonTorrent.Info.splitPieces()
+	assert.Nil(t, err)
+	for i, hash := range tf.PieceHashes {
+		for j, hashByte := range hash {
+			assert.Equal(t, fmt.Sprintf("%02X", hashByte), splitPieces[i*20+j])
+			assert.Equal(t, hashByte, pieceHashes[i][j])
+		}
+	}
+
+}
